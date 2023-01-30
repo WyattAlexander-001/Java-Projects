98cac4d3e7fb021d5a9298032c3dfa159b5bf330
Wyatt B <wyattbushman117@gmail.com>
2023-01-29 22:08:00 -0500

    Drew sprite, can move was WASD

--- PlatformerGame/src/main/GamePanel.java
+++ PlatformerGame/src/main/GamePanel.java
@@ -1,9 +1,13 @@
 package main;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.util.Random;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
@@ -15,29 +19,42 @@ public class GamePanel extends JPanel{
 	private MouseInputs mouseInputs;
 	private float xDelta = 100;
 	private float yDelta = 100;
-	private float xDir = 1f;
-	private float yDir = 1f;
-	private int frames = 0;
-	private long lastCheck = 0;
-	private Color color = new Color(0xD0A384);
-	private Random random;
+	private BufferedImage img , subImg;
+
+
+	
 	public GamePanel() {
-		random = new Random();
+
 		mouseInputs = new  MouseInputs(this);
+		importImage();
+		setPanelSize();
 		addKeyListener(new KeyboardInputs(this));
 		addMouseListener(mouseInputs);
 		addMouseMotionListener(mouseInputs);
 	}
 	
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g); //Call superclass JPanel paint component method
+	private void importImage() {
+		InputStream is = getClass().getResourceAsStream("/player_sprites.png"); //access resources, "/" is used to get to the folder at the same level of src 
+		try {
+			img = ImageIO.read(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-		updateRectangle();
-		g.setColor(color); //hex-color is 0xhex
-		g.fillRect((int)xDelta,(int)yDelta, 100, 13);
+	private void setPanelSize() {
+		Dimension size = new Dimension(1280, 800); // 1280/32 = 40 images wide and 800/32 = 25 images in height
+		//setMinimumSize(size);
+		//setMaximumSize(size);
+		setPreferredSize(size);
 		
 
-		//repaint();
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g); //Call superclass JPanel paint component method 
+		g.drawImage(img.getSubimage(0, 0, 64, 40), (int)xDelta, (int)yDelta, 128,80,null); //width is 64, height 40 due to sprite sheet. 128,80 is to double size.
+	
 	}
 	
 	public void changeXDelta(int value) {
@@ -56,25 +73,16 @@ public class GamePanel extends JPanel{
 		repaint();
 	}
 	
-	public void updateRectangle() {
-		xDelta += xDir;
-		if(xDelta >400 ||xDelta <0) {
-			xDir *=-1;
-			color = getRandomColor();
-		}
-		
-		yDelta+=yDir;
-		if(yDelta >400 ||yDelta <0) {
-			yDir *=-1;
-			color = getRandomColor();
-		}		
-	}
-
+	/* 
+	 * Method to include RGB 
 	private Color getRandomColor() {
 		int r = random.nextInt(0,255); //0-255
 		int b = random.nextInt(0,255);
 		int g = random.nextInt(0,255);
 		return new Color(r,b,g);
 	}
+	*/
+	
+	
 }
 
