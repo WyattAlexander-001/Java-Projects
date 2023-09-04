ced1d713d6029fc6f2e5926d5791a459bd68b861
Wyatt B <wyattbushman117@gmail.com>
2023-09-04 16:58:04 -0400

    Finished basic gui app
    can take user input and output to console

--- JavaFXRepo/src/application/Main.java
+++ JavaFXRepo/src/application/Main.java
@@ -1,26 +1,46 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            BorderPane root = new BorderPane();
+            Scene scene = new Scene(root, 640, 480);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setTitle("Hello World!");
+            primaryStage.setScene(scene);
+
+            Text txt = new Text("SUP? This is your first JavaFX");
+            TextField nameFld = new TextField();
+            //root.setCenter(txt);
+
+            Button btn = new Button();
+            String name = "Wyatt";
+            btn.setText("Howdy, Click here and it will output to console: " + name);
+            btn.setOnAction(evt -> System.out.printf("You clicked the button: %s%n", nameFld.getText())); //Outputs to console using lambda exp
+            //root.setTop(btn);
+
+            VBox box = new VBox();
+            box.getChildren().addAll(txt,  nameFld,btn);
+            root.setCenter(box);
+   
+            
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
