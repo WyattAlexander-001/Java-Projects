2ec096e22a50bfd29cb463f6b6763bf5229f24e7
Wyatt B <wyattbushman117@gmail.com>
2023-09-04 00:34:02 -0400

    JavaFX Folder added
    Added beginner folder for javafx. Used swing before but I need to be using modern tech.

---
+++ JavaFX/src/Beginner/Main.java
@@ -0,0 +1,10 @@
+package Beginner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("JavaFX is made of scenes");
+		System.out.println("We add nodes to scenes");
+	}
+
+}
