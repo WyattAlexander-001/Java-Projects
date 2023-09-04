2aa7d0eb8d5d5cbbaac76b61b5e17bef5018c81d
Wyatt B <wyattbushman117@gmail.com>
2023-09-04 17:09:15 -0400

    Added a new README
    I hate having to configure every time. I want it to be simple, but documenting will help future me!

---
+++ JavaFXRepo/README.md
@@ -0,0 +1,11 @@
+# https://www.youtube.com/watch?v=nz8P528uGjk
+
+# Right click project go to run-as configurations.
+
+* Go to arguments, paste this:
+```
+--module-path "H:\ProgramFiles\eclipse\openjfx-20.0.2_windows-x64_bin-sdk\javafx-sdk-20.0.2\lib" --add-modules javafx.controls,javafx.graphics,javafx.fxml
+```
+
+* Go to dependencies, move JavaFX to ModulePath Entries and out of classpath. 
+* Simply click on Modulepath Entries then go to advanced and use "Add library" -> User Library -> JavaFX
---
+++ JavaFXRepo/src/basicOutput/Main.java
@@ -0,0 +1,46 @@
+package basicOutput;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            BorderPane root = new BorderPane();
+            Scene scene = new Scene(root, 640, 480);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setTitle("Hello World!");
+            primaryStage.setScene(scene);
+
+            Text txt = new Text("SUP? This is your first JavaFX");
+            TextField nameFld = new TextField();
+            //root.setCenter(txt);
+
+            Button btn = new Button();
+            String name = "Wyatt";
+            btn.setText("Howdy, Click here and it will output to console: " + name);
+            btn.setOnAction(evt -> System.out.printf("You clicked the button: %s%n", nameFld.getText())); //Outputs to console using lambda exp
+            //root.setTop(btn);
+
+            VBox box = new VBox();
+            box.getChildren().addAll(txt,btn);
+            root.setCenter(box);
+   
+            
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
