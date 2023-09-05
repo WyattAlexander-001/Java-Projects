b4fde04387482d3b6fe27a9582fbfdd0c586370a
Wyatt B <wyattbushman117@gmail.com>
2023-09-04 23:40:20 -0400

    i

--- JavaFXRepo/src/application/Main.java
+++ JavaFXRepo/src/application/Main.java
@@ -1,6 +1,8 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -16,6 +18,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
+        	//Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
             BorderPane root = new BorderPane();
             Scene scene = new Scene(root, 640, 480);
             scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
